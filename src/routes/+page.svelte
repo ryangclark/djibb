d5c43290c94cb111656c69729e0b4d4affc322f8
Ryan Clark <rclark010@gmail.com>
2024-03-20 11:51:20 -0600

    Tailwind init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>djibb</h1>
+<p class="ml-8">Building beautiful, remixable checklists.</p>
